74d82f7eb406b7844e1e70e2d84100edb67a7e1c
Kook <47762427+CFTBL@users.noreply.github.com>
2026-05-27 15:59:19 -0700

    Update RPU_PinballPoolGameV90.ino

--- RPU_PinballPoolGameV90.ino
+++ RPU_PinballPoolGameV90.ino
@@ -126,7 +126,11 @@ unsigned long HighScore = 0;
 unsigned long AwardScores[3];           // Score thresholds for awards
 int Credits = 0;
 int MaximumCredits = 20;
-boolean FreePlayMode = false;
+
+// CFTBL - Change initial FreePlayMode setting to true
+// boolean FreePlayMode = false;
+boolean FreePlayMode = true;
+
 boolean MatchFeature = true;            //  Allows Match Feature to run
 
 #define MAX_TILT_WARNINGS_MAX    2
